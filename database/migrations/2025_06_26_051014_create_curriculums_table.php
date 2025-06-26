8f9b189d9d40a771e87eed6a617ce85dacc43bd6
matsuyama <success.is.doing@gmail.com>
2025-06-27 00:39:04 +0900

    マイグレーションファイルの作成curriculums_table＆delivery_times_table

---
+++ database/migrations/2025_06_26_051014_create_curriculums_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curriculums', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('thumbnail', 255);
+            $table->longText('description')->nullable();
+            $table->mediumText('video_url')->nullable();
+            $table->tinyInteger('always_delivery_flg')->default(0);
+            $table->unsignedBigInteger('grade_id');
+            $table->timestamps();
+
+            $table->foreign('grade_id')->references('id')->on('grades')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curriculums');
+    }
+};
