444f697c256a835d3509d3866e163570156f3ebc
kanta91 <slamdunk10takekan@gmail.com>
2025-07-11 22:23:05 +0900

    Add migration files from team_develop branch

---
+++ database/migrations/2025_06_25_071313_create_classes_clear_checks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes_clear_checks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('grade_id')->constrained()->onDelete('cascade');
+            $table->boolean('clear_flg')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes_clear_checks');
+    }
+};
