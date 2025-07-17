042cb68404b820fbe15721367cee503bc27c79ec
kanta91 <slamdunk10takekan@gmail.com>
2025-07-17 19:31:03 +0900

    バリデーションをリクエストファイルに設定　DBトランザクション設定

---
+++ app/Http/Requests/StoreArticleRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreArticleRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'posted_date' => 'required|date',
+            'article_contents' => 'required|string',
+        ];
+    }
+}
+
---
+++ app/Http/Requests/UpdateArticleRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateArticleRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'posted_date' => 'required|date',
+            'article_contents' => 'required|string',
+        ];
+    }
+}
